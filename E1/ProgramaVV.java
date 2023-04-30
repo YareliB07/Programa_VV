c90a2ff6660404ac1e7deb437e2e825868c91aab
YareliB07 <yarelibravo84@gmail.com>
2023-04-30 13:38:08 -0600

    Correcion de erroes, version 2

--- E1/ProgramaVV.java
+++ E1/ProgramaVV.java
@@ -2,11 +2,13 @@ package E1;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
-public class Programa_VV {
+public class ProgramaVV {
+  private static final Logger logger = Logger.getLogger(ProgramaVV.class.getName());
 
   public static void main(String[] args) {
-    ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+    ArrayList<MenuItem> menuItems = new ArrayList<>();
     menuItems.add(new MenuItem("Hamburguesa", 10, 1));
     menuItems.add(new MenuItem("Papas", 5, 1));
     menuItems.add(new MenuItem("Bebida", 2, 1));
@@ -15,28 +17,28 @@ public class Programa_VV {
     Scanner scanner = new Scanner(System.in);
 
     while (true) {
-      System.out.println("1. Imprimir menu");
-      System.out.println("2. Agregar orden");
-      System.out.println("3. Ver ordenes");
-      System.out.println("4. Calcular ingresos totales");
-      System.out.println("5. Exit");
-      System.out.print("Introduzca su opcion: ");
+      logger.info("1. Imprimir menu");
+      logger.info("2. Agregar orden");
+      logger.info("3. Ver ordenes");
+      logger.info("4. Calcular ingresos totales");
+      logger.info("5. Exit");
+      logger.info("Introduzca su opcion: ");
       int choice = scanner.nextInt();
 
       if (choice == 1) {
         restaurant.imprimirMenu();
       }
       else if (choice == 2) {
-        System.out.print("Ingrese el nombre de usuario: ");
+        logger.info("Ingrese el nombre de usuario: ");
         String customerName = scanner.next();
-        ArrayList<OrderItem> items = new ArrayList<OrderItem>();
+        ArrayList<OrderItem> items = new ArrayList<>();
         while (true) {
-          System.out.print("Ingrese el nombre del item (ingresa 0 para detener): ");
+          logger.info("Ingrese el nombre del item (ingresa 0 para detener): ");
           String itemName = scanner.next();
           if (itemName.equalsIgnoreCase("0")) {
             break;
           }
-          System.out.print("Ingrese cantidad: ");
+          logger.info("Ingrese cantidad: ");
           int cantidad = scanner.nextInt();
           for (MenuItem item : menuItems) {
             if (item.nombre.equals(itemName)) {
@@ -45,18 +47,22 @@ public class Programa_VV {
             }
           }
         }
-        Order orden = new Order(customerName, items);
-        restaurant.agregarOrden(orden);
+        Order order = new Order(customerName, items);
+        restaurant.agregarOrden(order);
       }
       else if (choice == 3) {
         restaurant.verOrdenes();
       }
       else if (choice == 4) {
         int totalRevenue = restaurant.calcularGananciaTotal();
-        System.out.println("Ganancia total: $" + totalRevenue);
+        logger.info("Total revenue: $" + totalRevenue);
       }
       else if (choice == 5) {
+        scanner.close();
         break;
+      }
+      else {
+        logger.warning("Opcion invalida");
       }
     }
   }
--- E1/Restaurant.java
+++ E1/Restaurant.java
@@ -1,62 +1,65 @@
 package E1;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-class Restaurant {
+public class Restaurant {
+    private static final Logger logger = Logger.getLogger(Restaurant.class.getName());
     ArrayList<MenuItem> menuItems;
     ArrayList<Order> orders;
-  
-    public Restaurant(ArrayList<MenuItem> menuItems) {
-      this.menuItems = menuItems;
-      this.orders = new ArrayList<Order>();
+
+    public Restaurant(List<MenuItem> menuItems) {
+        this.menuItems = (ArrayList<MenuItem>) menuItems;
+        this.orders = new ArrayList<>();
     }
-  
+
     public void agregarOrden(Order order) {
-      this.orders.add(order);
+        this.orders.add(order);
     }
-  
+
     public void imprimirMenu() {
-      for (MenuItem item : menuItems) {
-        System.out.println(item.nombre + ": $" + item.precio);
-      }
+        for (MenuItem item : menuItems) {
+            logger.log(Level.INFO, "{0}: ${1}", new Object[]{item.nombre, item.precio});
+        }
     }
-  
+
     public void verOrdenes() {
-      for (int i = 0; i < orders.size(); i++) {
-        Order order = orders.get(i);
-        System.out.println("Customer name: " + order.customerName);
-        System.out.println("Items:");
-        for (OrderItem item : order.items) {
-          System.out.println(item.nombre + " x" + item.cantidad + " $" + item.precio * item.cantidad);
+        for (int i = 0; i < orders.size(); i++) {
+            Order order = orders.get(i);
+            logger.log(Level.INFO, "Customer name: {0}", order.customerName);
+            logger.log(Level.INFO, "Items:");
+            for (OrderItem item : order.items) {
+                logger.log(Level.INFO, "{0} x{1} ${2}", new Object[]{item.nombre, item.cantidad, item.precio * item.cantidad});
+            }
+            logger.log(Level.INFO, "Total price: ${0}", order.calcularPrecioTotal());
         }
-        System.out.println("Total price: $" + order.calcularPrecioTotal());
-      }
     }
-  
+
     public int calcularGananciaTotal() {
-      int totalRevenue = 0;
-      for (Order order : orders) {
-        totalRevenue += order.calcularPrecioTotal();
-      }
-      return totalRevenue;
+        int totalRevenue = 0;
+        for (Order order : orders) {
+            totalRevenue += order.calcularPrecioTotal();
+        }
+        return totalRevenue;
     }
-  
+
     public int calcularMenuItemCantidad(String itemName) {
-      for (MenuItem item : menuItems) {
-        if (item.nombre.equals(itemName)) {
-          return item.cantidad;
+        for (MenuItem item : menuItems) {
+            if (item.nombre.equals(itemName)) {
+                return item.cantidad;
+            }
         }
-      }
-      return 0;
+        return 0;
     }
-  
+
     public void actualizaMenuItemCantidad(String itemName, int newQuantity) {
-      for (MenuItem item : menuItems) {
-        if (item.nombre.equals(itemName)) {
-          item.cantidad = newQuantity;
-          break;
+        for (MenuItem item : menuItems) {
+            if (item.nombre.equals(itemName)) {
+                item.cantidad = newQuantity;
+                break;
+            }
         }
-      }
     }
 }
-
